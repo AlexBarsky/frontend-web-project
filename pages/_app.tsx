ddb39960c757d9c1352dc9346c58fc64986ee4f9
AlexBarsky <bogomolov2004alex@gmail.com>
2024-04-06 03:31:26 -0700

    auth provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,14 +12,21 @@ const queryClient = new QueryClient({
 	},
 });
 
+import AuthProvider from '@/providers/auth-provider/AuthProvider';
+import { TypeComponentAuthFields } from '@/providers/auth-provider/auth-page.types';
 import { persistor, store } from '@/store/store';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+	Component,
+	pageProps,
+}: AppProps & TypeComponentAuthFields) {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<Provider store={store}>
 				<PersistGate loading={null} persistor={persistor}>
-					<Component {...pageProps} />
+					<AuthProvider Component={{ isOnlyUser: Component.isOnlyUser }}>
+						<Component {...pageProps} />
+					</AuthProvider>
 				</PersistGate>
 			</Provider>
 		</QueryClientProvider>
